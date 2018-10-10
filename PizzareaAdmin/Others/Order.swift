c75b9505c4d62947a8e1fcb234f50cddfa7923d3
Ryan James Paed Apellanes <43224497+RyanJamesPaedApellanes@users.noreply.github.com>
2018-10-10 06:41:58 -0700

    Add files via upload

---
+++ PizzareaAdmin/Others/Order.swift
@@ -0,0 +1,22 @@
+﻿//
+//  Order.swift
+//  PizzareaAdmin
+//
+//  Created by Ryan Apellanes on 04/02/2018.
+//  Copyright © 2018 CreativityKills Co. All rights reserved.
+//
+
+import Foundation
+
+struct Order {
+    let id: String
+    let pizza: Pizza
+    var status: OrderStatus
+}
+
+enum OrderStatus: String {
+    case pending = "Pending"
+    case accepted = "Accepted"
+    case dispatched = "Dispatched"
+    case delivered = "Delivered"
+}
---
+++ PizzareaAdmin/Others/Pizza.swift
@@ -0,0 +1,26 @@
+﻿//
+//  Pizza.swift
+//  PizzareaAdmin
+//
+//  Created by Ryan Apellanes on 04/02/2018.
+//  Copyright © 2018 CreativityKills Co. All rights reserved.
+//
+
+import UIKit
+
+struct Pizza {
+    let id: String
+    let name: String
+    let description: String
+    let amount: Float
+    let image: UIImage
+    
+    init(data: [String: Any]) {
+        self.id = data["id"] as! String
+        self.name = data["name"] as! String
+        self.amount = data["amount"] as! Float
+        self.description = data["description"] as! String
+        self.image = data["image"] as! UIImage
+    }
+}
+
